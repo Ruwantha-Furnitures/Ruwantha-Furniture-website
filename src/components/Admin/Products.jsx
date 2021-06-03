fe63bd550c14b66bed23d478b03d83a2f2345fae
WTGihan <wtgihan@gmail.com>
2021-06-03 20:40:47 +0530

    products view page complete

--- src/components/Admin/Products.jsx
+++ src/components/Admin/Products.jsx
@@ -91,15 +91,46 @@ function Products() {
                       <td>Rs.250</td>
                       <td>No.12</td>
                     </tr>
-                    <tr>
-                      <td className="moreInfo">Desk(Plastic)</td>
-                      <td>Product Type 1</td>
-                      <td>Rs.25000</td>
-                      <td>Rs.250</td>
-                      <td>No.12</td>
-                    </tr>
                   </tbody>
                 </table>
+              </div>
+              {/* Pagination */}
+              <div className="tablePagination">
+                <a href="#" className="paginationLink">
+                  <span class="material-icons paginationArrowIcon">
+                    arrow_back_ios
+                  </span>
+                </a>
+                <a href="#" className="paginationLink">
+                  <span className="material-icons paginationCircleIcon active">
+                    circle
+                  </span>
+                </a>
+                <a href="#" className="paginationLink">
+                  <span className="material-icons paginationCircleIcon">
+                    circle
+                  </span>
+                </a>
+                <a href="#" className="paginationLink">
+                  <span className="material-icons paginationCircleIcon">
+                    circle
+                  </span>
+                </a>
+                <a href="#" className="paginationLink">
+                  <span className="material-icons paginationCircleIcon">
+                    circle
+                  </span>
+                </a>
+                <a href="#" className="paginationLink">
+                  <span className="material-icons paginationCircleIcon">
+                    circle
+                  </span>
+                </a>
+                <a href="#" className="paginationLink">
+                  <span class="material-icons paginationArrowIcon">
+                    arrow_forward_ios
+                  </span>
+                </a>
               </div>
             </div>
           </div>
