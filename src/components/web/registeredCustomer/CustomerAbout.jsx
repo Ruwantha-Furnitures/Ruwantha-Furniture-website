a87d5f8d6665e53ea7c8cfe6224a8516238039ad
Pathmila <asinipathmila@gmail.com>
2021-06-13 17:43:32 +0530

    profile

--- src/components/web/registeredCustomer/CustomerAbout.jsx
+++ src/components/web/registeredCustomer/CustomerAbout.jsx
@@ -12,8 +12,9 @@ function CustomerAbout(props) {
         <div>
             <Navigation></Navigation>   
             <h2>About</h2>
-            <p>Mauris sem velit, vehicula eget sodales vitae,
-            rhoncus eget sapien:</p>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
             <ol>          
             <li>Facilisis bibendum</li>
             <li>Vestibulum vulputate</li>
