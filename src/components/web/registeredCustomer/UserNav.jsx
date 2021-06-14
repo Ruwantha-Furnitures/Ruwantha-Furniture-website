eb18ad403ffd6b6895a9fa13d92f46520b305e06
Pathmila <asinipathmila@gmail.com>
2021-06-14 15:41:24 +0530

    app.js

--- src/components/web/registeredCustomer/UserNav.jsx
+++ src/components/web/registeredCustomer/UserNav.jsx
@@ -12,12 +12,12 @@ import {
 } from "react-bootstrap";
 import logo from "../../../assets/logo.png";
 import ShoppingCart from "./Cart";
-<<<<<<< HEAD
+
 import Person from "./PersonIcon";
 import PropTypes from 'prop-types';
-=======
+
 import PropTypes from "prop-types";
->>>>>>> 9ddd21e54f96ae4e1a573a0977954fe2e413fc53
+
 import NavButtonStyle from "../../../css/web/common.module.css";
 import "../../../css/web/cart.css";
 
@@ -38,8 +38,6 @@ function UserNav(props) {
       cursor: pointer;
     }
   `;
-
-<<<<<<< HEAD
       const HoverText1 = styled.p`
         color: #fff;
         :hover {
@@ -82,61 +80,6 @@ function UserNav(props) {
             <br />    
         </div>
     );
-=======
-  const HoverText1 = styled.p`
-    color: #fff;
-    :hover {
-      color: #fc7a30;
-      cursor: pointer;
-    }
-  `;
-  return (
-    <div>
-      <Navbar
-        variant="dark"
-        expand="lg"
-        sticky="top"
-        style={{ backgroundColor: "#291B1B", maxWidth: "100%" }}
-      >
-        <Navbar.Brand href="#home" style={{ marginTop: "-20px" }}>
-          <img src={logo} alt={logo} width={100} height={100}></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/chome" style={navlink}>
-              <HoverText1>Home</HoverText1>
-            </Nav.Link>
-            <Nav.Link href="/cproduct" style={navlink}>
-              <HoverText1>Product</HoverText1>
-            </Nav.Link>
-            <Nav.Link href="/cabout-us" style={navlink}>
-              <HoverText1>About Us</HoverText1>
-            </Nav.Link>
-            <Nav.Link href="/ccontact-us" style={navlink}>
-              <HoverText1>Contact Us</HoverText1>
-            </Nav.Link>
-          </Nav>
-          <Form inline>
-            <Row>
-              <Col>
-                <Link to="/login">
-                  <ShoppingCart></ShoppingCart>
-                </Link>
-              </Col>
-              <Col>
-                <Link to="/login">
-                  <button className={NavButtonStyle.btn}>Logout</button>
-                </Link>
-              </Col>
-            </Row>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-      <br />
-    </div>
-  );
->>>>>>> 9ddd21e54f96ae4e1a573a0977954fe2e413fc53
 }
 
 export default UserNav;
