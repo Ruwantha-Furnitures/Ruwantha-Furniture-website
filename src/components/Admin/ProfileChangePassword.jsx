4397899439285407b498ca0cff0d493dcc66e50e
WTGihan <wtgihan@gmail.com>
2021-06-14 16:43:57 +0530

    profile change password update and completed

--- src/components/Admin/ProfileChangePassword.jsx
+++ src/components/Admin/ProfileChangePassword.jsx
@@ -4,8 +4,7 @@ import Sidebar from "./common/Sidebar";
 import AllProductsView from "./common/AllProductsView";
 import MainStyle from "../../css/dashboard/Main.module.css";
 import ProductStyle from "../../css/dashboard/Products.module.css";
-import ProductSellForm from "./product/ProductSellForm";
-import ProductViewFormStyle from "../../css/dashboard/product/ProductViewForm.module.css";
+import ProfileChangePasswordForm from "./profile/ProfileChangePasswordForm";
 
 function ProfileChangePassword() {
   return (
@@ -22,75 +21,8 @@ function ProfileChangePassword() {
         <div className={ProductStyle.cardDataSection}>
           <div className={ProductStyle.detailsSection}>
             <div className={ProductStyle.detailCard}>
-              {/* Product Sell Form Part */}
-              <React.Fragment>
-                <div>
-                  <h1 className={ProductViewFormStyle.titleStyle}>
-                    Profile Change Password
-                  </h1>
-                </div>
-                <form action="#" className={ProductViewFormStyle.formStyle}>
-                  <div className={ProductViewFormStyle.details}>
-                    <div className={ProductViewFormStyle.infoPart}>
-                      <div className={ProductViewFormStyle.form}>
-                        <div
-                          className={
-                            ProductViewFormStyle.formLine +
-                            " " +
-                            ProductViewFormStyle.setMarginTop
-                          }
-                        >
-                          <div className={ProductViewFormStyle.data}>
-                            <label className={ProductViewFormStyle.labelStyle}>
-                              Username
-                            </label>
-                            <input
-                              type="text"
-                              value=""
-                              placeholder="User Email"
-                              className={ProductViewFormStyle.inputStyle}
-                            />
-                          </div>
-                        </div>
-                        <div className={ProductViewFormStyle.formLine}>
-                          <div className={ProductViewFormStyle.data}>
-                            <label className={ProductViewFormStyle.labelStyle}>
-                              Current
-                            </label>
-                            <input
-                              type="text"
-                              value=""
-                              placeholder="Current Password"
-                              className={ProductViewFormStyle.inputStyle}
-                            />
-                          </div>
-                          <div className={ProductViewFormStyle.data}>
-                            <label className={ProductViewFormStyle.labelStyle}>
-                              New
-                            </label>
-                            <input
-                              type="text"
-                              value=""
-                              placeholder="New Password"
-                              className={ProductViewFormStyle.inputStyle}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className={ProductViewFormStyle.descButtonsAdd}>
-                    <div className={ProductViewFormStyle.descButtonAdd}>
-                      <button
-                        className={ProductViewFormStyle.descButtonAddStyle}
-                      >
-                        Change Password
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </React.Fragment>
+              {/* Profiel change password */}
+              <ProfileChangePasswordForm />
             </div>
           </div>
           <div className={ProductStyle.productsViewSection}>
