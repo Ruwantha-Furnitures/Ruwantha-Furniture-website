345c419b2d8234e8f22dca3e305155b2f5d2f8d2
WTGihan <wtgihan@gmail.com>
2021-06-19 17:53:33 +0530

    file format Change

--- src/components/Dashboard/ProductTypeAdd.jsx
+++ src/components/Dashboard/ProductTypeAdd.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./common/Sidebar";
 import AllProductsView from "./common/AllProductsView";
 import MainStyle from "../../css/dashboard/Main.module.css";
 import ProductStyle from "../../css/dashboard/Products.module.css";
-import ProductViewFormStyle from "../../css/dashboard/product/ProductViewForm.module.css";
+import ProductTypeAddForm from "./product/ProductTypeAddForm";
 
 function ProductTypeAdd() {
   return (
@@ -22,122 +22,7 @@ function ProductTypeAdd() {
           <div className={ProductStyle.detailsSection}>
             <div className={ProductStyle.detailCard}>
               {/* Product Type Form Part */}
-              <React.Fragment>
-                <div>
-                  <h1 className={ProductViewFormStyle.titleStyle}>
-                    Product Type Add
-                  </h1>
-                </div>
-                <form action="#">
-                  <div className={ProductViewFormStyle.details}>
-                    <div className={ProductViewFormStyle.imgDescPart}>
-                      <div className={ProductViewFormStyle.typeForm}>
-                        <div className={ProductViewFormStyle.descTitle}>
-                          <div
-                            className={ProductViewFormStyle.dataProductTitle}
-                          >
-                            <label
-                              className={ProductViewFormStyle.labelProductTitle}
-                            >
-                              Category
-                            </label>
-                            <select
-                              className={ProductViewFormStyle.inputProductTitle}
-                            >
-                              <option value="">Select Category</option>
-                              <option value="category1">Category 1</option>
-                              <option value="category2">Category 2</option>
-                              <option value="category3">Category 3</option>
-                              <option value="category4">Category 4</option>
-                            </select>
-                          </div>
-                          <div
-                            className={ProductViewFormStyle.dataProductTitle}
-                          >
-                            <label
-                              className={ProductViewFormStyle.labelProductTitle}
-                            >
-                              New Type
-                            </label>
-                            <input
-                              type="text"
-                              value=""
-                              placeholder="New Product Type"
-                              className={ProductViewFormStyle.inputProductTitle}
-                            />
-                          </div>
-                          <div
-                            className={ProductViewFormStyle.descButtonsAddType}
-                          >
-                            <div className={ProductViewFormStyle.descButtonAdd}>
-                              <button
-                                className={
-                                  ProductViewFormStyle.descButtonAddStyle
-                                }
-                              >
-                                Add Type
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className={ProductViewFormStyle.typesList}>
-                        <h1 className={ProductViewFormStyle.productTypeTitle}>
-                          Category 01
-                        </h1>
-                        <div className={ProductViewFormStyle.productType}>
-                          <span
-                            class={
-                              "material-icons " + ProductViewFormStyle.iconSize
-                            }
-                          >
-                            circle
-                          </span>
-                          <h1 className={ProductViewFormStyle.productTypeName}>
-                            Type 01
-                          </h1>
-                        </div>
-                        <div className={ProductViewFormStyle.productType}>
-                          <span
-                            class={
-                              "material-icons " + ProductViewFormStyle.iconSize
-                            }
-                          >
-                            circle
-                          </span>
-                          <h1 className={ProductViewFormStyle.productTypeName}>
-                            Type 02
-                          </h1>
-                        </div>
-                        <div className={ProductViewFormStyle.productType}>
-                          <span
-                            class={
-                              "material-icons " + ProductViewFormStyle.iconSize
-                            }
-                          >
-                            circle
-                          </span>
-                          <h1 className={ProductViewFormStyle.productTypeName}>
-                            Type 02
-                          </h1>
-                        </div>
-                        <div className={ProductViewFormStyle.productType}>
-                          <span
-                            class={
-                              "material-icons " + ProductViewFormStyle.iconSize
-                            }
-                          >
-                            circle
-                          </span>
-                          <h1 className={ProductViewFormStyle.productTypeName}>
-                            Type 02
-                          </h1>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </form>
-              </React.Fragment>
+              <ProductTypeAddForm />
             </div>
           </div>
           <div className={ProductStyle.productsViewSection}>
