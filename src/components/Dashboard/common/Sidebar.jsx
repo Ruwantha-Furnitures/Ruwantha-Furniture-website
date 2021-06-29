b5150897800e7e6364a95286364fe58965704c9e
WTGihan <wtgihan@gmail.com>
2021-06-29 17:56:38 +0530

    complete complete order page

--- src/components/Dashboard/common/Sidebar.jsx
+++ src/components/Dashboard/common/Sidebar.jsx
@@ -71,6 +71,23 @@ function Sidebar() {
           </li>
           <li>
             <Link
+              to="/dashboard/completedOrders"
+              className={SidebarStyle.sidebarLink}
+            >
+              <div className={SidebarStyle.lineSection}>
+                <div className={SidebarStyle.lineIcon}>
+                  <span className={"material-icons " + SidebarStyle.iconWidth}>
+                    fact_check
+                  </span>
+                </div>
+                <div className={SidebarStyle.lineText}>
+                  <h1 className={SidebarStyle.lineTextStyle}>Completed </h1>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link
               to="/dashboard/deliveryDrivers"
               className={SidebarStyle.sidebarLink}
             >
---
+++ src/components/Dashboard/order/CompleteOrderList.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import TableStyle from "../../../css/dashboard/Table.module.css";
+
+function CompleteOrderList() {
+  return (
+    <React.Fragment>
+      <div className={TableStyle.tabletitle}>
+        <h1 className={TableStyle.tableTitleProductStyle}>Completed Orders</h1>
+      </div>
+      <div className={TableStyle.tablebody}>
+        <table className={TableStyle.tableShow}>
+          <thead>
+            <tr>
+              <th>Order Id</th>
+              <th>Customer</th>
+              <th>Contact Number</th>
+              <th>Date</th>
+              <th>Driver</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:1</td>
+              <td>Tharindu Gihan</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Gihan Withanchchi</td>
+            </tr>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:2</td>
+              <td>Himasha Anjali</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Himasha Anjali</td>
+            </tr>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:3</td>
+              <td>Sathira Dimuthu</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Sathira Dimuthu</td>
+            </tr>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:4</td>
+              <td>Anushaka Tharindu</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Anushka Tharindu</td>
+            </tr>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:5</td>
+              <td>Tharindu Gihan</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Tharindu Gihan</td>
+            </tr>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:6</td>
+              <td>Thanuj Dasun</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Dimuthu Ranthansinghe</td>
+            </tr>
+            <tr>
+              <td className={TableStyle.moreInfo}>No:7</td>
+              <td>Dimuthu Rathnasinghe</td>
+              <td>0778522736</td>
+              <td>2021-03-17</td>
+              <td className={TableStyle.moreInfo}>Tharindu Gihan</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={TableStyle.tablePagination}>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span className={"material-icons " + TableStyle.paginationArrowIcon}>
+            arrow_back_ios
+          </span>
+        </Link>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span
+            className={
+              "material-icons " +
+              TableStyle.paginationCircleIcon +
+              " " +
+              TableStyle.active
+            }
+          >
+            circle
+          </span>
+        </Link>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span className={"material-icons " + TableStyle.paginationCircleIcon}>
+            circle
+          </span>
+        </Link>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span className={"material-icons " + TableStyle.paginationCircleIcon}>
+            circle
+          </span>
+        </Link>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span className={"material-icons " + TableStyle.paginationCircleIcon}>
+            circle
+          </span>
+        </Link>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span className={"material-icons " + TableStyle.paginationCircleIcon}>
+            circle
+          </span>
+        </Link>
+        <Link to="#" className={TableStyle.paginationLink}>
+          <span className={"material-icons " + TableStyle.paginationArrowIcon}>
+            arrow_forward_ios
+          </span>
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default CompleteOrderList;
