1eed52db4a625ca743fd8eb748a9a868ff63642a
Pathmila <asinipathmila@gmail.com>
2021-07-01 01:41:21 +0530

    search Product form

--- src/components/web/registeredCustomer/CustomerProduct.jsx
+++ src/components/web/registeredCustomer/CustomerProduct.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Navigation from "./UserNav";
 import Footer from "../Common/Footer";
-import Form from "react-bootstrap/Form";
-import FormStyle from "../../../css/web/Form.module.css";
-import NavButtonStyle from "../../../css/web/common.module.css";
 import ProductBox from "./CustomerAllProducts";
 
 function CustomerProduct() {
@@ -14,22 +11,7 @@ function CustomerProduct() {
     }
     return (
         <div style={contactImg}>  
-        <Navigation></Navigation>  
-        <Container>
-            <Row sm={12}>
-                <Col sm={1}> 
-                    <label style={{marginTop: '10px'}}>Category</label>
-                </Col>
-                <Col sm={3}>  
-                <Form lassName={FormStyle.innerbox}>                    
-                    <select className={FormStyle.dropBox} >
-                        <option>Asini</option>
-                        <option>Asini</option>
-                    </select>
-                </Form>                
-                </Col>
-            </Row>     
-        </Container>       
+        <Navigation></Navigation>               
         <Container fluid align="center"> 
             <ProductBox></ProductBox>
         </Container>
