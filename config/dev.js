912e22430777a33f37a7e9d62b9cc3646d22e453
Pathmila <asinipathmila@gmail.com>
2021-07-04 02:41:04 +0530

    backend model, controller for signup

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://AsiniPathmila:<password>@cluster0.hizqj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- src/components/web/customer/Product.jsx
+++ src/components/web/customer/Product.jsx
@@ -4,12 +4,15 @@ import Navigation from "./Indexnav";
 import Footer from "../Common/Footer";
 import ProductBox from "./AllProductCards";
 import Search from "../Common/SearchProduct";
+import Card from 'react-bootstrap/Card';
 import Topimg from '../../../assets/background.jpg';
+import Coverimg from '../../../assets/topimg20.jpg';
+import { CardImg } from 'react-bootstrap';
 
 function CustomerProduct() {
     require("bootstrap/dist/css/bootstrap.min.css");
     const contactImg = {  
-        //backgroundImage: `url(${Topimg})` ,      
+        //backgroundImage: `url(${Coverimg})` ,      
         backgroundColor: 'white',
         MaxWidth: "100%"
     }
@@ -22,10 +25,22 @@ function CustomerProduct() {
                     <Search></Search>
                 </Col>                
             </Row> 
-        </Container>                    
-        <Container fluid align="center"> 
-            <ProductBox></ProductBox>
-        </Container>
+        </Container> 
+        <Container>
+        <Row sm={12} align="justify">
+            <Col sm={12}>                
+                <Card>                    
+                    <Card.Img variant="top" src={Coverimg} />                    
+                    <Card.Body>
+                        <Container fluid align="center"> 
+                            <ProductBox></ProductBox>
+                        </Container>                        
+                    </Card.Body>
+                </Card>  
+                <br />
+            </Col> 
+        </Row> 
+        </Container>                                       
         <Footer></Footer>    
     </div>
     )
