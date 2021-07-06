6ccc4c276a88e61af2d9776ab259e7b6a7f99ee9
MedaniGunathilaka <hansikamedani@gmail.com>
2021-07-06 21:44:04 +0530

    Customer files were added

--- src/components/web/customer/Signup.jsx
+++ src/components/web/customer/Signup.jsx
@@ -1,44 +1,39 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Navigation from "./Indexnav";
 import Footer from "../Common/Footer";
 import SignForm from "./SignupForm";
-import backcover from "../../../assets/login9.jpg";
+import backcover from "../../../assets/topimg27.jpg";
 import axios from "axios";
 
-const Signup = ({ navigation }) => {
+const Signup = ({navigation}) => {
   const signUpHandler = async (data) => {
     //console.log(data);
-    try {
-      await axios.post("http://192.168.56.1:3002/api/customer/signup", {
+    try{
+        await axios.post("http://192.168.56.1:3002/api/customer/signup",{
         data,
       });
       console.log("Request successful");
-    } catch (error) {
+    }catch (error){
       console.log(error);
-    }
+    } 
   };
-  return (
-    <div
-      style={{
-        backgroundImage: `url(${backcover})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        objectFit: "cover",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <Navigation></Navigation>
-      <Container align="center">
-        <SignForm
-          navigation={navigation}
-          signUpHandler={signUpHandler}
-        ></SignForm>
-      </Container>
-      <Footer></Footer>
-    </div>
-  );
-};
+    return (
+        <div style={{              
+            backgroundImage: `url(${backcover})`,        
+            backgroundRepeat: 'no-repeat',                        
+            backgroundSize: 'cover',
+            objectFit:'cover',
+            height: '100%',
+            width: '100%'    
+          }}>
+            <Navigation></Navigation>
+            <Container align="right"> 
+              <SignForm navigation={navigation} signUpHandler={signUpHandler} ></SignForm>   
+            </Container>                    
+            <Footer></Footer>       
+          </div>
+    );
+}
 
 export default Signup;
