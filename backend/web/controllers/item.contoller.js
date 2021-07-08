89d55d2bdfa71c0f8fd1f9a8bfda2b7bf808e5b4
MedaniGunathilaka <hansikamedani@gmail.com>
2021-07-08 09:34:47 +0530

    Added backend controller files

--- backend/web/controllers/item.contoller.js
+++ backend/web/controllers/item.contoller.js
@@ -1,48 +1,14 @@
 const { Item } = require("../models");
 const mysql = require("mysql");
 
-exports.allItems = (req, res) => {
-    // find all Item information from 
-    try{
-        Item.findAll({attributes: ['itemid', 'name', 'typid', 'price', 'quantity', "details", "objectid"]})
-        .then(allItems => {
-            res.status(200).json(allItems);
-        })
-    }catch(error) {
-        // log on console
-        console.log(error);
-
-        res.status(500).json({
-            message: "Error!",
-            error: error
-        });
-    }
+//get all employee list
+const ItemController = (req, res) => {
+    Item.getAllItem((err, items) => {
+        console.log("All Items");
+        if (err) res.send(err);
+        console.log("Items", items);
+        res.send(items);
+    });
 }
 
-// exports.createItem = (req, res) => {
-//     let item = {};
-
-//     try{
-//         // Building Customer object from upoading request's body
-//         item.itemid = req.body.itemid;
-//         item.name = req.body.name;
-//         item.typid = req.body.typid;
-//         item.price = req.body.price;
-//         item.quantity = req.body.quantity;
-//         item.details = req.body.details;
-//         item.objectid = req.body.objectid;
-    
-//         // Save to MySQL database
-//         Item.create(Item, 
-//                           {attributes: ['itemid', 'name', 'typid', 'price', 'quantity', "details", "objectid"]})
-//                     .then(result => {    
-//                       res.status(200).json(result);
-//                     });
-//     }catch(error){
-//         res.status(500).json({
-//             message: "Fail!",
-//             error: error.message
-//         });
-//     }
-// }
-
+module.exports = { ItemController };
