90cc7003c3c01013312cddb09a9836f39d2de7e1
Pathmila <asinipathmila@gmail.com>
2021-07-12 18:36:56 +0530

    Thanking Page Done

--- src/components/web/registeredCustomer/Payment/ThankYouPage.jsx
+++ src/components/web/registeredCustomer/Payment/ThankYouPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navigation from "../Navigation/UserNav";
 import Footer from "../../Common/Footer";
+import Topimg from '../../../../assets/topimg32.jpg';
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import { Container } from 'reactstrap';
 
 function ThankYouPage() {
     const contactImg = {
@@ -10,12 +14,27 @@ function ThankYouPage() {
         padding: '0',        
         MaxWidth: "100%"     
     }
+    const insidediv = {
+        backgroundColor: 'rgb(0,0,0,0.8)',
+        height: '100%',
+        width: '100%',
+        color: 'white',
+        padding: '70px',
+        justifyItem: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'        
+    }
     return (
         <div style={contactImg}>                                     
             <Navigation></Navigation>             
             <Container align='left'>
-                
-            </Container>                                                                                    
+                <Form style={insidediv}>
+                    <p style={{fontSize: '8rem'}}>Thank You!</p>
+                    <p style={{fontSize: '3rem'}}>Your Order ID: 45896588</p>
+                    <Button variant="success">Go to Home Page</Button>{' '}    
+                </Form>
+            </Container>  
+            <br />                                                                                  
             <Footer></Footer>    
         </div>
     )
