da2960ce03ac0dd0ed6ca1bd6b37a59519b1a92e
Nuwan Fernando <lifeofhassa@outlook.com>
2021-07-12 20:06:14 +0530

    Updation

--- backend/web/routes/Customers.js
+++ backend/web/routes/Customers.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 //const mysql = require("mysql");
 
-const { SignUpController} = require("../controllers/SignupController");
+const { SignUpController } = require("../controllers/SignupController");
 const { LoginController } = require("../controllers/login.controller");
+const {
+  ViewProfileController,
+} = require("../controllers/ViewProfileController");
 
 router.post("/signup", SignUpController);
 router.post("/login", LoginController);
+router.get("/viewprofile/:accID", ViewProfileController);
 
 module.exports = { customerRouter: router };
