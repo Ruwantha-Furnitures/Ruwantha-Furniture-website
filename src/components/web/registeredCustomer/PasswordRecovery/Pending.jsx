3a14aae3fd341e91da064260a43b29ce7c313813
Nuwan Fernando <lifeofhassa@outlook.com>
2021-07-12 19:49:57 +0530

    Update2

--- src/components/web/registeredCustomer/PasswordRecovery/Pending.jsx
+++ src/components/web/registeredCustomer/PasswordRecovery/Pending.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Form from "react-bootstrap/Form";
+import "../../../../css/web/Login.css";
+import Avatar from "../../../../assets/pwrecovery.png";
+import NavButtonStyle from "../../../../css/web/common.module.css";
+import FormStyle from "../../../../css/web/Form.module.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Pending() {
     return (
@@ -6,15 +13,10 @@ function Pending() {
             <Card className={FormStyle.cardbox} style={{marginTop:'30px', marginBottom: '30px', width: '22rem',border: 'solid 3px bisque', boxShadow:'0px 0px 20px #000'}}>      
             <Form style={{padding: '20px'}}>  
                 <center><img src={Avatar} style={avatar} alt='avatar'/></center><br />
-                <center><h3>Password Recovery</h3></center>
+                <center><h3>We sent an email to your account.</h3></center>
+                <center><h4>Please login into your email account and click on the link we sent to reset your password.</h4></center>
                           
-                <input  className={FormStyle.emailBox}
-                  type='email'
-                  placeholder='Enter your email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                ></input>              
+                             
                                           
                 <br /><br />
                 <div align="center">
