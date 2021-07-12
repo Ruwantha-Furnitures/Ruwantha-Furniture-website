cd0b6210c43a7ea4e546bc19c3a48a9a8b0d1d60
Nuwan Fernando <lifeofhassa@outlook.com>
2021-07-12 18:00:55 +0530

    updating thanking page

--- src/components/web/registeredCustomer/Payment/ThankYouPage.jsx
+++ src/components/web/registeredCustomer/Payment/ThankYouPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Navigation from "../Navigation/UserNav";
 import Footer from "../../Common/Footer";
-import Topimg from '../../../../assets/topimg31.jpg';
+import Topimg from '../../../../assets/topimg32.jpg';
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import { Container } from 'reactstrap';
 
 function ThankYouPage() {
     const contactImg = {
@@ -12,14 +15,26 @@ function ThankYouPage() {
         MaxWidth: "100%"     
     }
     const insidediv = {
-        backgroundColor: 'rgb(0,0,0,0.8)',
+        backgroundColor: 'rgb(0,0,0,0.7)',
+        height: '100%',
+        width: '100%',
+        color: 'white',
+        padding: '70px',
+        justifyItem: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'        
     }
     return (
         <div style={contactImg}>                                     
             <Navigation></Navigation>             
-            <Container align='left' style={insidediv} >
-                
-            </Container>                                                                                    
+            <Container fluid align='left'>
+                <Form style={insidediv}>
+                    <p style={{fontSize: '8rem'}}>Thank You!</p>
+                    <p style={{fontSize: '3rem'}}>Your Order ID: 45896588</p>
+                    <Button variant="success">Go to Home Page</Button>{' '}    
+                </Form>
+            </Container>  
+            <br />                                                                                  
             <Footer></Footer>    
         </div>
     )
