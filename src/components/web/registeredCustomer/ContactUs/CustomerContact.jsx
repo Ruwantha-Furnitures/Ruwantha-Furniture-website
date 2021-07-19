1d0c14d1ae93b0ba7cf5edb3c150e80646a58c3c
Pathmila <asinipathmila@gmail.com>
2021-07-20 02:05:27 +0530

    customer contact us form backend  done

--- src/components/web/registeredCustomer/ContactUs/CustomerContact.jsx
+++ src/components/web/registeredCustomer/ContactUs/CustomerContact.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navigation from "../Navigation/UserNav";
 import Footer from "../../Common/Footer";
 import About from "../../Common/About";
 import ConForm from "../../Common/ContactForm";
 import { Container} from 'reactstrap';
 import Topimg from '../../../../assets/topimg10.jpg';
+import axios from "axios";
+import { Redirect } from 'react-router-dom';
 
 function CustomerContact() {
+    const [isSubmit, setIsSubmit] = useState(false);
+    //console.log("test")
+    const contactUsHandler =async (data) =>{
+        try{            
+            const respond = await axios.post("http://192.168.56.1:3002/api/contactus/contact",
+                { data }
+            );
+            // console.log("asini")
+            if(respond.data.auth === true){
+                setIsSubmit(true);
+            }else{
+                setIsSubmit(false);
+            }            
+        }catch(error){
+            console.log(error);
+        }
+    };
+
     const contactImg = {
         backgroundImage: `url(${Topimg})` ,
         repeat: 'none',
@@ -14,17 +34,23 @@ function CustomerContact() {
         MaxWidth: "100%"
     }
 
-    return (        
-        <div>                                     
-            <Navigation></Navigation> 
-            <About></About>
-            <div style={contactImg}>
-                <Container align="left">
-                    <ConForm></ConForm>
-                </Container>
-            </div>            
-            <Footer></Footer>            
-        </div>                      
+    const redirectHome = <Redirect to="/home" />
+    return ( 
+        <React.Fragment>
+            {(isSubmit===true) && (redirectHome)}
+            {(isSubmit===false) && (
+                <div>                                     
+                    <Navigation></Navigation> 
+                    <About></About>
+                    <div style={contactImg}>
+                        <Container align="left">
+                            <ConForm contactUsHandler={contactUsHandler}></ConForm>
+                        </Container>
+                    </div>            
+                    <Footer></Footer>            
+                </div>  
+            )}           
+        </React.Fragment>                        
     );
 }
 
