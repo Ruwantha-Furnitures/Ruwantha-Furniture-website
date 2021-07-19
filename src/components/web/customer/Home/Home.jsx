29d2b53eb98cc79daaecc2ebf4f5bea03160f4e3
Pathmila <asinipathmila@gmail.com>
2021-07-20 00:51:12 +0530

    customize contactus form backend is progressing

--- src/components/web/customer/Home/Home.jsx
+++ src/components/web/customer/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container} from "reactstrap";
 import ConForm from "../../Common/CustomizeProduct";
 import Topimg from '../../../../assets/topimg19.jpg';
@@ -9,37 +9,62 @@ import Footer from "../../Common/Footer";
 import Card from 'react-bootstrap/Card';
 import Gallary from '../../Common/ImageGallary';
 import "../../../../css/web/Home.css";
-import CommonFormStyle from "../../../../css/web/common.module.css";
+import axios from "axios";
+import  { Redirect } from 'react-router-dom';
 
-function Home() {
-  require("bootstrap/dist/css/bootstrap.min.css");
-  const contactImg = {
-    backgroundImage: `url(${Topimg})` , 
-    repeat: 'none',    
-    MaxWidth: "100%"
-  };
+const Home = () => {
+    require("bootstrap/dist/css/bootstrap.min.css");
+    const [isContactSubmit, setIsContactSubmit] = useState(false);
+    
+    const customizeHandler = async(data) =>{
+        try{
+            const response = await axios.post("http://192.168.56.1:3002/api/contactus/contact",
+                {data}
+            );
+            if(response.data.auth == true){
+                setIsContactSubmit(true);
+            }else{
+                setIsContactSubmit(false);
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
 
-  return (
-    <div>
-      <Navigation></Navigation>
-      <Slideshow></Slideshow>
-      <br/>
-      <Container fluid><Card>
-          <br/><Card.Title><center><h2>Furniture Items</h2></center></Card.Title><br />
-          <ProductBox></ProductBox> 
-      </Card></Container> 
-      <br />        
-      <Container fluid><Gallary></Gallary></Container>      
-      <div style={contactImg}>
-          <br />
-          <Container align="left">                         
-            <ConForm></ConForm>              
-          </Container>   
-          <br />     
-      </div> 
-      <Footer></Footer>
-    </div>
-  );
+    const contactImg = {
+        backgroundImage: `url(${Topimg})` , 
+        repeat: 'none',    
+        MaxWidth: "100%"
+    };
+
+    const redirectHome = <Redirect to="/home" /> 
+    return (
+        <React.Fragment>
+            {(isContactSubmit === true) && (redirectHome)}
+            {(isContactSubmit === false) && (
+                <div>
+                    <Navigation></Navigation>
+                    <Slideshow></Slideshow>
+                    <br/>
+                    <Container fluid><Card>
+                        <br/><Card.Title><center><h2>Furniture Items</h2></center></Card.Title><br />
+                        <ProductBox></ProductBox> 
+                    </Card></Container> 
+                    <br />        
+                    <Container fluid><Gallary></Gallary></Container>      
+                    <div style={contactImg}>
+                        <br />
+                        <Container align="left">                         
+                          <ConForm customizeHandler={customizeHandler}></ConForm>              
+                        </Container>   
+                        <br />     
+                    </div> 
+                    <Footer></Footer>
+                </div>
+            )}
+        
+        </React.Fragment>
+    );
 }
 
 export default Home;
