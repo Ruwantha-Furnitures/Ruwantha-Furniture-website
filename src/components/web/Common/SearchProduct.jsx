f2aa988b4fe72864ff60cc3c682a867cf595a85d
MedaniGunathilaka <hansikamedani@gmail.com>
2021-08-21 12:06:54 +0530

    Updated common files

--- src/components/web/Common/SearchProduct.jsx
+++ src/components/web/Common/SearchProduct.jsx
@@ -1,15 +1,23 @@
 import React, {useState,useEffect} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+import {Row} from "reactstrap";
+import Button from "react-bootstrap/Button";
+// import FormControl from 'react-bootstrap/FormControl';
 import axios from 'axios';
 
-function SearchProduct() {    
+const SearchProduct = ({DropDownHandler}) => {    
     require("bootstrap/dist/css/bootstrap.min.css");
     const [allcategory,setAllCategory]=useState([])    
-    const [category, setCategory] = useState("");     
+    const [category, setCategory] = useState("");    
+    
+
+    const submithandler = (e) => {
+        console.log(category)
+        DropDownHandler(category)     
+    }
 
     useEffect(() => {
         getCategories();
@@ -42,22 +50,25 @@ function SearchProduct() {
             <Navbar expand="lg"  sticky="top" style={{ maxWidth:'100%', marginTop:'0px', marginBottom: '20px'}}>            
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Form inline>
-                    <select style={textboxStyle} onChange={(e)=>setCategory(e.target.value)}>
-                        <option style={{color:'red'}} value="" disabled selected hidden >Select category</option>
-                        {allcategory.map((categoryList) =>(                                          
-                            <option value={categoryList.name}>{categoryList.name}</option>
-                        ))}
-                    </select>
+                <Form inline onSubmit={submithandler}>
+                    <Row>
+                        <select style={textboxStyle} onChange={(e)=>setCategory(e.target.value)}>
+                            <option style={{color:'red'}} value="" disabled selected hidden >Select category</option>
+                            {allcategory.map((categoryList) =>(                                          
+                                <option value={categoryList.id}>{categoryList.name}</option>
+                            ))}
+                        </select>
+                        <Button variant="success" type='submit'>Submit</Button>{' '}
+                    </Row>                     
                 </Form>
-                <Form inline> 
+                {/* <Form inline> 
                     <input style={textboxStyle}
                         type='text' 
                         placeholder='Search' 
                         //value={address}
                         //onChange={(e)=>setAddress(e.target.value)}                        
                     ></input>                   
-                </Form>
+                </Form> */}
             </Navbar.Collapse>
             </Navbar>                       
         </div>
