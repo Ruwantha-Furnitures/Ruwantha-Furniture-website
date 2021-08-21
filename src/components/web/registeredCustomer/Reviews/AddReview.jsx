29c3d78cfa65ce39e26b623ed3409c8aa47c1269
Nuwan Fernando <lifeofhassa@outlook.com>
2021-08-21 15:47:44 +0530

    src web files

--- src/components/web/registeredCustomer/Reviews/AddReview.jsx
+++ src/components/web/registeredCustomer/Reviews/AddReview.jsx
@@ -2,18 +2,47 @@ import React , {  useState } from 'react';
 import Navigation from "../Navigation/UserNav";
 import Footer from "../../Common/Footer";
 import Button from 'react-bootstrap/Button';
-import Rating from './StarRating';
-import Subnavigation from "../Navigation/Subnav";
 import Form from "react-bootstrap/Form";
 import {Container , Row} from 'reactstrap';
 import Card from 'react-bootstrap/Card';
 import FormStyle from "../../../../css/web/Form.module.css";
-import productImg from "../../../../assets/items/2.jpg";
 import Topimg from '../../../../assets/topimg39.png';
+import { Rating } from '@material-ui/lab';
+import Box from "@material-ui/core/Box";
+import axios from "axios";
+import { useEffect } from 'react';
 
 function AddReview() {
-    const [description, setDescription] = useState("");
+    const [product_id, setProductID] = useState("");
+    const [feedback, setDescription] = useState("");
+    const [rating_points, setRating] = React.useState(1);
+
+    const [reviewProduct, setReviewProduct] = useState("");
+
     
+    useEffect(() => {
+        fetchProductData()
+    }, [])
+
+    const submitHandler = async(e) => {       
+        e.preventDefault();
+        //data from the form
+        const data = { product_id, feedback, rating_points };
+        
+        console.log(rating_points)
+        
+        
+      };
+    
+    const fetchProductData = async() => {
+        let id =localStorage.getItem('ReviewProductID');  
+        setProductID(id)
+
+        const orderResponse = await axios.get(`http://localhost:8080/api/product/${id}`)
+        console.log(orderResponse.data) 
+        setReviewProduct(orderResponse.data)       
+    }
+
     const title={
         margin: '4px',
         padding: '3px',
@@ -45,19 +74,29 @@ function AddReview() {
             
             <Container>
             <Card className={FormStyle.cardbox} style={{marginBottom: '20px', width: '21rem',border: 'solid 3px bisque', boxShadow:'0px 0px 20px #000'}}>      
-            <Form style={{padding: '20px'}}>   
+            <Form style={{padding: '20px'}} onSubmit={submitHandler}>   
                 <Row style={rowStyle}>                                                                      
                     <h3 style={title}>Rating & Feedback</h3>
                 </Row>                                                                         
                 <center>
-                    <img src={productImg} style={{width:'100px', borderRadius: '20px'}} alt='imgitem'></img><br /><br />
-                    <h5>Pearl Wooden Dining Chair</h5>
-                    <Rating></Rating>
+                    <img src={reviewProduct.img_location} style={{width:'100px', borderRadius: '20px'}} alt='imgitem'></img><br /><br />
+                    <h5>{reviewProduct.name}</h5>
+
+                    <Box component="fieldset" mb={3} borderColor="transparent">                        
+                        <Rating
+                        name="simple-controlled"
+                        value={rating_points}
+                        onChange={(event, newValue) => {
+                            setRating(newValue);
+                        }}
+                        />
+                    </Box>
+
                     <textarea style={textareaStyle} 
                         rows={5} 
                         cols={5}                             
                         placeholder='Enter your feedback here.'
-                        value={description}    
+                        value={feedback}    
                         onChange={(e)=>setDescription(e.target.value)}  
                         required
                     ></textarea><br /> 
