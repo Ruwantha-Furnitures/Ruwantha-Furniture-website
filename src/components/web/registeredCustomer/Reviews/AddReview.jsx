badd2ad65fe67a59b5b06754e7e49f589b475363
Nuwan Fernando <lifeofhassa@outlook.com>
2021-08-21 17:14:24 +0530

    updation - Web

--- src/components/web/registeredCustomer/Reviews/AddReview.jsx
+++ src/components/web/registeredCustomer/Reviews/AddReview.jsx
@@ -1,5 +1,6 @@
 import React , {  useState } from 'react';
 import Navigation from "../Navigation/UserNav";
+import Subnavigation from "../Navigation/Subnav";
 import Footer from "../../Common/Footer";
 import Button from 'react-bootstrap/Button';
 import Form from "react-bootstrap/Form";
@@ -11,6 +12,7 @@ import { Rating } from '@material-ui/lab';
 import Box from "@material-ui/core/Box";
 import axios from "axios";
 import { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function AddReview() {
     const [product_id, setProductID] = useState("");
@@ -38,6 +40,7 @@ function AddReview() {
             console.log(response.data);
             if(response.status === 200){
                 setIsSubmit(true)
+                alert("Thank you for taking the time to give the product a rating.")
             }else{
                 setIsSubmit(false)
             }
@@ -85,47 +88,54 @@ function AddReview() {
         padding: '0',
         MaxWidth: "100%"     
     }
+
+    const redirectProductHistory = <Redirect to="/customer_reviews"></Redirect>;
     return (
-        <div className="col-md-12" style={contactImg}>
-            <Navigation></Navigation>
-            
-            <Container>
-            <Card className={FormStyle.cardbox} style={{marginBottom: '20px', width: '21rem',border: 'solid 3px bisque', boxShadow:'0px 0px 20px #000'}}>      
-            <Form style={{padding: '20px'}} onSubmit={submitHandler}>   
-                <Row style={rowStyle}>                                                                      
-                    <h3 style={title}>Rating & Feedback</h3>
-                </Row>                                                                         
-                <center>
-                    <img src={reviewProduct.img_location} style={{width:'100px', borderRadius: '20px'}} alt='imgitem'></img><br /><br />
-                    <h5>{reviewProduct.name}</h5>
-
-                    <Box component="fieldset" mb={3} borderColor="transparent">                        
-                        <Rating
-                        name="simple-controlled"
-                        value={rating_points}
-                        onChange={(event, newValue) => {
-                            setRating(newValue);
-                        }}
-                        />
-                    </Box>
-
-                    <textarea style={textareaStyle} 
-                        rows={5} 
-                        cols={5}                             
-                        placeholder='Enter your feedback here.'
-                        value={feedback}    
-                        onChange={(e)=>setDescription(e.target.value)}  
-                        required
-                    ></textarea><br /> 
-                </center>   
-                <div align="right"><br />                                   
-                    <Button variant='success' type='submit'>Submit</Button> 
+        <React.Fragment>
+            {(isSubmit === true) && (redirectProductHistory)}
+            {(isSubmit === false) && (
+                <div className="col-md-12" style={contactImg}>
+                    <Navigation></Navigation>
+                    <Subnavigation></Subnavigation>
+                    <Container>
+                    <Card className={FormStyle.cardbox} style={{marginBottom: '20px', width: '21rem',border: 'solid 3px bisque', boxShadow:'0px 0px 20px #000'}}>      
+                    <Form style={{padding: '20px'}} onSubmit={submitHandler}>   
+                        <Row style={rowStyle}>                                                                      
+                            <h3 style={title}>Rating & Feedback</h3>
+                        </Row>                                                                         
+                        <center>
+                            <img src={reviewProduct.img_location} style={{width:'100px', borderRadius: '20px'}} alt='imgitem'></img><br /><br />
+                            <h5>{reviewProduct.name}</h5>
+        
+                            <Box component="fieldset" mb={3} borderColor="transparent">                        
+                                <Rating
+                                name="simple-controlled"
+                                value={rating_points}
+                                onChange={(event, newValue) => {
+                                    setRating(newValue);
+                                }}
+                                />
+                            </Box>
+        
+                            <textarea style={textareaStyle} 
+                                rows={5} 
+                                cols={5}                             
+                                placeholder='Enter your feedback here.'
+                                value={feedback}    
+                                onChange={(e)=>setDescription(e.target.value)}  
+                                required
+                            ></textarea><br /> 
+                        </center>   
+                        <div align="right"><br />                                   
+                            <Button variant='success' type='submit'>Submit</Button> 
+                        </div>
+                    </Form>
+                    </Card>
+                    </Container>
+                    <Footer></Footer>
                 </div>
-            </Form>
-            </Card>
-            </Container>
-            <Footer></Footer>
-        </div>
+            )}
+        </React.Fragment>        
     )
 }
 
