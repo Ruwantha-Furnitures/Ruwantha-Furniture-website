611007947a070743a1df657d0e76d5c200f949c6
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-04 16:12:32 +0530

    update final

--- src/components/Dashboard/common/TableCard.jsx
+++ src/components/Dashboard/common/TableCard.jsx
@@ -1,64 +1,64 @@
 import React from "react";
 import TableCardStyle from "../../../css/dashboard/Table.module.css";
 
-// function TableCard() {
-//   return (
-//     <div className={TableCardStyle.table}>
-//       <div className={TableCardStyle.tabletitle}>
-//         <h1 className={TableCardStyle.tableTitleStyle}>Top Selling Products</h1>
-//       </div>
-//       <div className={TableCardStyle.tablebody}>
-//         <table className={TableCardStyle.tableShow}>
-//           <thead>
-//             <tr>
-//               <th>Products</th>
-//               <th>Price</th>
-//               <th>Discount</th>
-//               <th>Sold</th>
-//               {/* <th>Method</th> */}
-//             </tr>
-//           </thead>
-//           <tbody>
-//             <tr>
-//               <td>Canon Suite Table</td>
-//               <td>Rs.25000</td>
-//               <td>20%</td>
-//               <td>12</td>
-//               {/* <td>Cash</td> */}
-//             </tr>
-//             <tr>
-//               <td>Red Suite Chair</td>
-//               <td>Rs.15000</td>
-//               <td>10%</td>
-//               <td>10</td>
-//               {/* <td>Online</td> */}
-//             </tr>
-//             <tr>
-//               <td>Indigo Delux Cupboard</td>
-//               <td>Rs.35000</td>
-//               <td>10%</td>
-//               <td>10</td>
-//               {/* <td>Cash</td> */}
-//             </tr>
-//             <tr>
-//               <td>Indigo Standard Desk</td>
-//               <td>Rs.25050</td>
-//               <td>10%</td>
-//               <td>09</td>
-//               {/* <td>Cash</td> */}
-//             </tr>
-//             <tr>
-//               <td>Indigo Standard Pro Desk</td>
-//               <td>Rs.55000</td>
-//               <td>09%</td>
-//               <td>08</td>
-//               {/* <td>Online</td> */}
-//             </tr>
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// }
+function TableCard() {
+  return (
+    <div className={TableCardStyle.table}>
+      <div className={TableCardStyle.tabletitle}>
+        <h1 className={TableCardStyle.tableTitleStyle}>Top Selling Products</h1>
+      </div>
+      <div className={TableCardStyle.tablebody}>
+        <table className={TableCardStyle.tableShow}>
+          <thead>
+            <tr>
+              <th>Products</th>
+              <th>Price</th>
+              <th>Discount</th>
+              <th>Sold</th>
+              {/* <th>Method</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Canon Suite Table</td>
+              <td>Rs.25000</td>
+              <td>20%</td>
+              <td>12</td>
+              {/* <td>Cash</td> */}
+            </tr>
+            <tr>
+              <td>Red Suite Chair</td>
+              <td>Rs.15000</td>
+              <td>10%</td>
+              <td>10</td>
+              {/* <td>Online</td> */}
+            </tr>
+            <tr>
+              <td>Indigo Delux Cupboard</td>
+              <td>Rs.35000</td>
+              <td>10%</td>
+              <td>10</td>
+              {/* <td>Cash</td> */}
+            </tr>
+            <tr>
+              <td>Indigo Standard Desk</td>
+              <td>Rs.25050</td>
+              <td>10%</td>
+              <td>09</td>
+              {/* <td>Cash</td> */}
+            </tr>
+            <tr>
+              <td>Indigo Standard Pro Desk</td>
+              <td>Rs.55000</td>
+              <td>09%</td>
+              <td>08</td>
+              {/* <td>Online</td> */}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
 
 export default TableCard;
