37cbae62f44fa852dcea7e78f6f457032dcc49e8
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-04 15:59:19 +0530

    update final

--- src/components/Dashboard/common/Chart.jsx
+++ src/components/Dashboard/common/Chart.jsx
@@ -2,106 +2,106 @@ import React from "react";
 import { BarChart, Bar, XAxis, Cell } from "recharts";
 import ChartStyle from "../../../css/dashboard/Chart.module.css";
 
-// function Chart() {
-//   const barColors = ["#542B14", "#B89068"];
-//   const data = [
-//     {
-//       name: "Jan",
-//       uv: 100,
-//       pv: 40,
-//       amt: 100,
-//     },
-//     {
-//       name: "Feb",
-//       uv: 100,
-//       pv: 1398,
-//       amt: 2210,
-//     },
-//     {
-//       name: "Mar",
-//       uv: 100,
-//       pv: 9800,
-//       amt: 2290,
-//     },
-//     {
-//       name: "Apr",
-//       uv: 100,
-//       pv: 3908,
-//       amt: 2000,
-//     },
-//     {
-//       name: "May",
-//       uv: 100,
-//       pv: 4800,
-//       amt: 2181,
-//     },
-//     {
-//       name: "Jun",
-//       uv: 2390,
-//       pv: 3800,
-//       amt: 2500,
-//     },
-//     {
-//       name: "Jul",
-//       uv: 3490,
-//       pv: 4300,
-//       amt: 2100,
-//     },
-//     {
-//       name: "Aug",
-//       uv: 2000,
-//       pv: 9800,
-//       amt: 2290,
-//     },
-//     {
-//       name: "Sep",
-//       uv: 100,
-//       pv: 0,
-//       amt: 2000,
-//     },
-//     {
-//       name: "Oct",
-//       uv: 100,
-//       pv: 0,
-//       amt: 2181,
-//     },
-//     {
-//       name: "Nov",
-//       uv: 100,
-//       pv: 0,
-//       amt: 2500,
-//     },
-//     {
-//       name: "Dec",
-//       uv: 100,
-//       pv: 4300,
-//       amt: 2100,
-//     },
-//   ];
-//   return (
-//     <div className={ChartStyle.graphChart}>
-//       <BarChart
-//         width={650}
-//         height={250}
-//         data={data}
-//         margin={{
-//           top: 5,
-//           right: 30,
-//           left: 20,
-//           bottom: 5,
-//         }}
-//         barSize={30}
-//       >
-//         <XAxis dataKey="name" size={30} axisLine={false} tickLine={false} />
-//         <Bar dataKey="uv" fill="#8884d8">
-//           {data.map((entry, index) => (
-//             <Cell key={`cell-${index}`} fill={barColors[index % 2]} />
-//           ))}
-//         </Bar>
-//       </BarChart>
-//       <h1 className={ChartStyle.chartLabel}>Sales of the Year</h1>
-//     </div>
-//   );
-// }
+function Chart() {
+  const barColors = ["#542B14", "#B89068"];
+  const data = [
+    {
+      name: "Jan",
+      uv: 100,
+      pv: 40,
+      amt: 100,
+    },
+    {
+      name: "Feb",
+      uv: 100,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Mar",
+      uv: 100,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Apr",
+      uv: 100,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "May",
+      uv: 100,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Jun",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Jul",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+      name: "Aug",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Sep",
+      uv: 100,
+      pv: 0,
+      amt: 2000,
+    },
+    {
+      name: "Oct",
+      uv: 100,
+      pv: 0,
+      amt: 2181,
+    },
+    {
+      name: "Nov",
+      uv: 100,
+      pv: 0,
+      amt: 2500,
+    },
+    {
+      name: "Dec",
+      uv: 100,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  return (
+    <div className={ChartStyle.graphChart}>
+      <BarChart
+        width={650}
+        height={250}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+        barSize={30}
+      >
+        <XAxis dataKey="name" size={30} axisLine={false} tickLine={false} />
+        <Bar dataKey="uv" fill="#8884d8">
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={barColors[index % 2]} />
+          ))}
+        </Bar>
+      </BarChart>
+      <h1 className={ChartStyle.chartLabel}>Sales of the Year</h1>
+    </div>
+  );
+}
 
 export default Chart;
