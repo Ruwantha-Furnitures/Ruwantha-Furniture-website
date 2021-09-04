f53629862c30fd4bfdf3faf745602ce8145b4cb7
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-04 16:16:46 +0530

    update final

--- src/components/Dashboard/common/TableCardDriver.jsx
+++ src/components/Dashboard/common/TableCardDriver.jsx
@@ -1,66 +1,66 @@
 import React from "react";
 import TableCardStyle from "../../../css/dashboard/Table.module.css";
 
-// function TableCardDriver() {
-//   return (
-//     <div className={TableCardStyle.table}>
-//       <div className={TableCardStyle.tabletitle}>
-//         <h1 className={TableCardStyle.tableTitleStyle}>
-//           Top Deliveries Products
-//         </h1>
-//       </div>
-//       <div className={TableCardStyle.tablebody}>
-//         <table className={TableCardStyle.tableShow}>
-//           <thead>
-//             <tr>
-//               <th>Product</th>
-//               <th>Price</th>
-//               <th>Discount</th>
-//               <th>Sold</th>
-//               <th>Source</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             <tr>
-//               <td>Table</td>
-//               <td>Rs.25000</td>
-//               <td>Rs.250</td>
-//               <td>No.12</td>
-//               <td>Direct</td>
-//             </tr>
-//             <tr>
-//               <td>Chair</td>
-//               <td>Rs.25000</td>
-//               <td>Rs.250</td>
-//               <td>No.12</td>
-//               <td>Online</td>
-//             </tr>
-//             <tr>
-//               <td>Cupboard</td>
-//               <td>Rs.25000</td>
-//               <td>Rs.250</td>
-//               <td>No.12</td>
-//               <td>Direct</td>
-//             </tr>
-//             <tr>
-//               <td>Desk</td>
-//               <td>Rs.25000</td>
-//               <td>Rs.250</td>
-//               <td>No.12</td>
-//               <td>Direct</td>
-//             </tr>
-//             <tr>
-//               <td>Desk(High)</td>
-//               <td>Rs.25000</td>
-//               <td>Rs.250</td>
-//               <td>No.12</td>
-//               <td>Online</td>
-//             </tr>
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// }
+function TableCardDriver() {
+  return (
+    <div className={TableCardStyle.table}>
+      <div className={TableCardStyle.tabletitle}>
+        <h1 className={TableCardStyle.tableTitleStyle}>
+          Top Deliveries Products
+        </h1>
+      </div>
+      <div className={TableCardStyle.tablebody}>
+        <table className={TableCardStyle.tableShow}>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Discount</th>
+              <th>Sold</th>
+              <th>Source</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Table</td>
+              <td>Rs.25000</td>
+              <td>Rs.250</td>
+              <td>No.12</td>
+              <td>Direct</td>
+            </tr>
+            <tr>
+              <td>Chair</td>
+              <td>Rs.25000</td>
+              <td>Rs.250</td>
+              <td>No.12</td>
+              <td>Online</td>
+            </tr>
+            <tr>
+              <td>Cupboard</td>
+              <td>Rs.25000</td>
+              <td>Rs.250</td>
+              <td>No.12</td>
+              <td>Direct</td>
+            </tr>
+            <tr>
+              <td>Desk</td>
+              <td>Rs.25000</td>
+              <td>Rs.250</td>
+              <td>No.12</td>
+              <td>Direct</td>
+            </tr>
+            <tr>
+              <td>Desk(High)</td>
+              <td>Rs.25000</td>
+              <td>Rs.250</td>
+              <td>No.12</td>
+              <td>Online</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
 
 export default TableCardDriver;
