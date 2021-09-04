a4137baa9cda3179779220d96bbf7822ddb2ca85
Nuwan Fernando <lifeofhassa@outlook.com>
2021-09-04 15:49:41 +0530

    update 2

--- src/components/Dashboard/DashboardDriver.jsx
+++ src/components/Dashboard/DashboardDriver.jsx
@@ -8,64 +8,64 @@ import ChartDriver from "./common/ChartDriver";
 import PieChartCardDriver from "./common/PieChartCardDriver";
 import TableCardDriver from "./common/TableCardDriver";
 
-// function DashboardDriver() {
-//   return (
-//     <React.Fragment>
-//       <div className={MainStyle.bodycontainer}>
-//         <div className={MainStyle.navSection}>
-//           <Navbar />
-//         </div>
-//         {/* Body */}
-//         <div className={MainStyle.bodySection}>
-//           {/* Sidebar */}
-//           <div className={MainStyle.sidebarSection}>
-//             <Sidebar />
-//           </div>
-//           <div className={DashboardStyle.cardSection}>
-//             <div className={DashboardStyle.detailsCardSection}>
-//               <DetailCard
-//                 detailCardMargin={true}
-//                 materialIconName={"local_shipping"}
-//                 cardText={"Today Deliveries"}
-//                 cardPrice={"No.5"}
-//               />
-//               <DetailCard
-//                 detailCardMargin={true}
-//                 materialIconName={"category"}
-//                 cardText={"All Deliveries"}
-//                 cardPrice={"No.10"}
-//               />
-//               <DetailCard
-//                 detailCardMargin={true}
-//                 materialIconName={"stacked_line_chart"}
-//                 cardText={"Daily Income"}
-//                 cardPrice={"Rs.25,000"}
-//               />
-//               <DetailCard
-//                 detailCardMargin={false}
-//                 materialIconName={"spa"}
-//                 cardText={"Total Income"}
-//                 cardPrice={"Rs.2,500,000"}
-//               />
-//             </div>
-//             <div className={DashboardStyle.chartCardSection}>
-//               <div className={DashboardStyle.graphTableSection}>
-//                 <div className={DashboardStyle.graphSection}>
-//                   <ChartDriver />
-//                 </div>
-//                 <div className={DashboardStyle.tableSection}>
-//                   <TableCardDriver />
-//                 </div>
-//               </div>
-//               <div className={DashboardStyle.circleGraphSection}>
-//                 <PieChartCardDriver />
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
+function DashboardDriver() {
+  return (
+    <React.Fragment>
+      <div className={MainStyle.bodycontainer}>
+        <div className={MainStyle.navSection}>
+          <Navbar />
+        </div>
+        {/* Body */}
+        <div className={MainStyle.bodySection}>
+          {/* Sidebar */}
+          <div className={MainStyle.sidebarSection}>
+            <Sidebar />
+          </div>
+          <div className={DashboardStyle.cardSection}>
+            <div className={DashboardStyle.detailsCardSection}>
+              <DetailCard
+                detailCardMargin={true}
+                materialIconName={"local_shipping"}
+                cardText={"Today Deliveries"}
+                cardPrice={"No.5"}
+              />
+              <DetailCard
+                detailCardMargin={true}
+                materialIconName={"category"}
+                cardText={"All Deliveries"}
+                cardPrice={"No.10"}
+              />
+              <DetailCard
+                detailCardMargin={true}
+                materialIconName={"stacked_line_chart"}
+                cardText={"Daily Income"}
+                cardPrice={"Rs.25,000"}
+              />
+              <DetailCard
+                detailCardMargin={false}
+                materialIconName={"spa"}
+                cardText={"Total Income"}
+                cardPrice={"Rs.2,500,000"}
+              />
+            </div>
+            <div className={DashboardStyle.chartCardSection}>
+              <div className={DashboardStyle.graphTableSection}>
+                <div className={DashboardStyle.graphSection}>
+                  <ChartDriver />
+                </div>
+                <div className={DashboardStyle.tableSection}>
+                  <TableCardDriver />
+                </div>
+              </div>
+              <div className={DashboardStyle.circleGraphSection}>
+                <PieChartCardDriver />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
 
 // export default DashboardDriver;
