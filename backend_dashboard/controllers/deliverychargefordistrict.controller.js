25e2ba27557f39f61ffb019a985f8efbba06a799
WTGihan <wtgihan@gmail.com>
2021-09-20 19:37:28 +0530

    group24

--- backend_dashboard/controllers/deliverychargefordistrict.controller.js
+++ backend_dashboard/controllers/deliverychargefordistrict.controller.js
@@ -3,17 +3,18 @@ const DeliveryCharges = db.deliveryCharges;
 
 // retrieve the data
 exports.findOne = (req, res) => {
-    const area = req.params.area;   
+  const area = req.params.area;
 
-    DeliveryCharges.findOne({ where: { area: area }})
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occured while retrieving DeliveryChargess",
-        });
+  DeliveryCharges.findOne({
+    where: { area: area },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while retrieving DeliveryChargess",
       });
-  };
-  
+    });
+};
